9600733f1d87c71a4fd0655685fd6ce88af8e739
Straxis Red <straxisred@straxiss-imac-3.local>
2018-06-25 18:12:09 +0530

    StudentZone New module implemented Dynamically from Firebase

--- DrApp/DrApp/homeViewController.m
+++ DrApp/DrApp/homeViewController.m
@@ -12,6 +12,7 @@
 #import "MultimediaViewController.h"
 #import "ContactViewController.h"
 #import "AppointmentsViewController.h"
+#import "StudentsZoneViewController.h"
 
 @interface homeViewController ()
 {
@@ -75,6 +76,8 @@
      withBlock:^(FIRDataSnapshot *snapshot) {
           for (NSArray * stArr in snapshot.value) {
              [arrModules addObject:stArr];
+              NSLog(@"%@",arrModules);
+
          }
          [_collectionView reloadData];
      }withCancelBlock:^(NSError * error){
@@ -230,12 +233,17 @@
     }
     else if (indexPath.row == 7)
     {
-        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil
+        StudentsZoneViewController * studentsZone = [[StudentsZoneViewController alloc] init];
+        [self.navigationController pushViewController:studentsZone animated:YES];
+        
+       /* UIAlertView *alert = [[UIAlertView alloc] initWithTitle:nil
                                                         message:@"Under Development"
                                                        delegate:self
                                               cancelButtonTitle:@"OK"
                                               otherButtonTitles:nil];
         [alert show];
+        */
+        
     }
     else if (indexPath.row == 8)
     {
